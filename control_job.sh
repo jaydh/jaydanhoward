ef8d92d2c3702c816b0128050d9ffef75b5322e9
Jay Howard <hello@jaydanhoward.com>
2023-08-18 10:37:54 -0700

    make lighthouse simply wait for health check

--- control_job.sh
+++ control_job.sh
@@ -2,11 +2,33 @@
 
 set -m
 
+# start app
 ./jaydanhoward &
 
-sleep 30s
-(cd /home/chrome && runuser -l  chrome -c 'lighthouse --chrome-flags="--headless" https://jaydanhoward-29npi.ondigitalocean.app') 
+APP_URL="https://jaydanhoward-29npi.ondigitalocean.app"  # Replace with your actual health check URL
+MAX_RETRIES=10  # Maximum number of retries
+RETRY_INTERVAL=5  # Time interval between retries in seconds
+
+retry_count=0
+
+while [ $retry_count -lt $MAX_RETRIES ]; do
+    response_code=$(curl -s -o /dev/null -w "%{http_code}" $APP_URL/health_check)
+    
+    if [ "$response_code" -eq 200 ]; then
+        echo "Health check successful (HTTP 200)."
+        break
+    else
+        echo "Health check returned HTTP $response_code. Retrying in $RETRY_INTERVAL seconds..."
+        sleep $RETRY_INTERVAL
+        retry_count=$((retry_count + 1))
+    fi
+done
+
+if [ $retry_count -eq $MAX_RETRIES ]; then
+    echo "Health check did not succeed after $MAX_RETRIES retries."
+fi
+
+(cd /home/chrome && runuser -l  chrome -c 'lighthouse --chrome-flags="--headless" $APP_URL') 
 cp /home/chrome/jaydanhoward*.report.html /app/site/lighthouse.html
-sleep 10m
 
 fg %1
