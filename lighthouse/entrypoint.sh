9c4a2ad9dd32acecf90e057636b809797805a9c6
Jay Howard <hello@jaydanhoward.com>
2023-08-18 20:55:57 -0700

    fix lightroom url in entrypoint

--- lighthouse/entrypoint.sh
+++ lighthouse/entrypoint.sh
@@ -6,7 +6,7 @@ MAX_RETRIES=10  # Maximum number of retries
 RETRY_INTERVAL=5  # Time interval between retries in seconds
 retry_count=0
 
-echo "Starting lighthouse worker"
+echo "Starting lighthouse worker with target $LEPTOS_SITE_ADDR" 
 while [ $retry_count -lt $MAX_RETRIES ]; do
     response_code=$(curl -s -o /dev/null -w "%{http_code}" $LEPTOS_SITE_ADDR/health_check)
     
@@ -25,5 +25,5 @@ if [ $retry_count -eq $MAX_RETRIES ]; then
     exit 1
 fi
 
-lighthouse --chrome-flags="--headless" http://$LEPTOS_SITE_ADDR/about
+lighthouse --chrome-flags="--headless" $LEPTOS_SITE_ADDR/about
 exit 0
