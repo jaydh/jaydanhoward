d4ab4d609894c2e583031b2671128542d664844a
Jay Howard <hello@jaydanhoward.com>
2023-08-18 22:00:17 -0700

    add file upload endpoint

--- src/routes/lighthouse/post.rs
+++ src/routes/lighthouse/post.rs
@@ -1,12 +1,21 @@
 #[cfg(feature = "ssr")]
-use actix_web::{web, HttpResponse};
-
-#[derive(serde::Deserialize)]
-pub struct FormData {}
+use {
+    actix_multipart::Multipart,
+    actix_web::{Error, HttpResponse},
+    futures_util::StreamExt as _,
+};
 
 #[cfg(feature = "ssr")]
-pub async fn upload_lighthouse_report(
-    form: web::Form<FormData>,
-) -> Result<HttpResponse, actix_web::Error> {
-    Ok(HttpResponse::Ok().finish())
+pub async fn upload_lighthouse_report(mut payload: Multipart) -> Result<HttpResponse, Error> {
+    // iterate over multipart stream
+    while let Some(item) = payload.next().await {
+        let mut field = item?;
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            println!("-- CHUNK: \n{:?}", std::str::from_utf8(&chunk?));
+        }
+    }
+
+    Ok(HttpResponse::Ok().into())
 }
