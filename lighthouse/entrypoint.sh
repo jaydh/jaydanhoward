03442e205aa2f914343d93843a9cd173f247fc18
Jay Howard <hello@jaydanhoward.com>
2023-08-19 01:22:13 -0700

    strip hyphens from uuid

--- lighthouse/entrypoint.sh
+++ lighthouse/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-IFS=$' \t\r\n'
 set -m
 
 MAX_RETRIES=10  # Maximum number of retries
@@ -28,5 +27,6 @@ fi
 lighthouse --output-path lighthouse.html --chrome-flags="--headless" $LEPTOS_SITE_ADDR/about
 
 BOUNDARY_UUID=$(uuidgen)
-curl -X POST -H "Content-Type: multipart/form-data; boundary=$BOUNDARY_UUID" "file=@/home/chrome/lighthouse.html" "$LEPTOS_SITE_ADDR/api/lighthouse"
+BOUNDARY_UUID_NO_HYPHENS=${BOUNDARY_UUID//-}
+curl -X POST -H "Content-Type: multipart/form-data; boundary=$BOUNDARY_UUID_NO_HYPHENS" "file=@/home/chrome/lighthouse.html" "$LEPTOS_SITE_ADDR/api/lighthouse"
 exit 0
