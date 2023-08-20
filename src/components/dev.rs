c2e2ee389459af51064e75ef58e75a708c1d0149
Jay Howard <hello@jaydanhoward.com>
2023-08-20 11:38:05 -0700

    use absolute path for lighthouse report

--- src/components/dev.rs
+++ src/components/dev.rs
@@ -4,14 +4,12 @@ use leptos::*;
 pub fn Lighthouse(cx: Scope) -> impl IntoView {
     view! { cx,
 
-        <h2 class="font-semibold">r#"Programmatically generated lighthosue report("#
-            <a class="underline" href="https://github.com/jaydh/jaydanhoward/blob/main/lighthouse/entrypoint.sh" target="_blank" rel="noreferrer">
-                for every deploy
-            </a>
+        <div>r#"Programmatically generated lighthouse report("#
+            <a class="font-semibold underline" href="https://github.com/jaydh/jaydanhoward/blob/main/lighthouse/entrypoint.sh" target="_blank" rel="noreferrer">for every deploy</a>
              r#")"#. Note that the performance score here suffers from running chrome on small instance size, my local machine regularly get 99+.
-        </h2>
+        </div>
         <iframe
-            src="assets/lighthouse.html"
+            src="/assets/lighthouse.html"
             title="Lighthouse Report"
             class="w-full h-4/6"
         />
@@ -21,7 +19,7 @@ pub fn Lighthouse(cx: Scope) -> impl IntoView {
 #[component]
 pub fn Github(cx: Scope) -> impl IntoView {
     view! { cx,
-         <div class="flex flex-col">
+         <div class="flex flex-col font-semibold">
              <a class="underline" href="https://github.com/jaydh/jaydanhoward" target="_blank" rel="noreferrer">Source code</a>
              <a class="underline" href="https://github.com/jaydh/jaydanhoward/actions" target="_blank" rel="noreferrer">Latest deploys</a>
          </div>
