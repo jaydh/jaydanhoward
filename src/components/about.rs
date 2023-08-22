bf190afb177e8083a86543df574f538a31b0f14e
Jay Howard <hello@jaydanhoward.com>
2023-08-22 12:44:11 -0700

    add fa and contact links

--- src/components/about.rs
+++ src/components/about.rs
@@ -5,9 +5,14 @@ pub fn About(cx: Scope) -> impl IntoView {
     view! {
         cx,
         <div class="flex flex-row">
-            <div>
+            <div class="flex flex-col space-y-10">
                 <h1 class="text-5xl font-heavy mb-6">r#"👋I'm Jay Dan Howard! I believe compassion makes tech worthwhile."#</h1>
                 <p>"Very few things are good in and of themselves, and tech is probably not one of them. I'm currently a senior software engineer at Interwell Health, leading an engineering team where we use software to empower clinicians and nephrologists to treat and prevent kidney disease. I try to keep a low-key life and avoid the spotlight but with that said, I plan to change the world."
+                </p>
+                <p>
+                    r#"This site exists to experiment with tech (currently that's Rust + Leptos + Tailwind), and to have a small corner of the internet where people can learn about me
+                        (mostly in a software engineering context). I live in beautiful San Francisco.
+                        I spend my AFK time walking my dog Lunabelle, wrenching on my motorcycle, and mindfully engaging in sillyness."#
                 </p>
             </div>
             <img src="/assets/profile.jpg" class="pl-20"/>
--- src/components/app.rs
+++ src/components/app.rs
@@ -10,28 +10,56 @@ pub fn App(cx: Scope) -> impl IntoView {
     provide_meta_context(cx);
 
     let routes = vec![("/about", "About"), ("/skills", "Skills"), ("/dev", "Dev")];
+    let links = vec![
+        ("https://github.com/jaydh", "fa-brands fa-github-square"),
+        (
+            "https://www.linkedin.com/in/jaydanhoward/",
+            "fa-brands fa-linkedin",
+        ),
+        ("mailto:hello@jaydanhoward.com", "fa-solid fa-envelope"),
+    ];
 
     view! { cx,
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
+        <Stylesheet id="fa" href="/assets/fontawesome/css/fontawesome.min.css"/>
+        <Stylesheet id="fa-brands" href="/assets/fontawesome/css/brands.min.css"/>
+        <Stylesheet id="fa-solid" href="/assets/fontawesome/css/solid.min.css"/>
         <Title text="Jay Dan Howard"/>
         <Router>
             <main>
                 <div class="flex w-screen h-full bg-pale-beige px-40">
                     <div class="flex flex-col w-full min-h-screen gap-10 bg-ivory-beige px-40 pb-40">
                         <nav class="pointer-events-auto hidden md:block mt-20 mb-20">
-                            <ul class="flex rounded-full bg-warm-beige px-3 text-sm font-medium">
-                                {routes.into_iter()
-                                    .map(|(route, display_text)| {
-                                        view! { cx,
-                                            <a
-                                                href=route
-                                                class="hover:underline relative block px-3 py-2 transition"
-                                            >
-                                                {display_text}
-                                            </a>
-                                        }
-                                    })
-                                    .collect_view(cx)}
+                            <ul class="flex flex-row rounded-full bg-warm-beige px-3 text-sm font-medium">
+                                <ul class="flex">
+                                    {routes.into_iter()
+                                        .map(|(route, display_text)| {
+                                            view! { cx,
+                                                <a
+                                                    href=route
+                                                    class="hover:underline relative block px-3 py-2 transition"
+                                                >
+                                                    {display_text}
+                                                </a>
+                                            }
+                                        })
+                                        .collect_view(cx)}
+                                </ul>
+                                <ul class="flex ml-auto">
+                                    {links.into_iter()
+                                        .map(|(route, iconClass)| {
+                                            view! { cx,
+                                                <a
+                                                    href=route
+                                                    class="hover:underline relative block px-3 py-2 transition"
+                                                    target="_blank" rel="noreferrer"
+                                                >
+                                                    <i class=iconClass />
+                                                </a>
+                                            }
+                                        })
+                                        .collect_view(cx)}
+                                </ul>
                             </ul>
                         </nav>
                         <Routes>
