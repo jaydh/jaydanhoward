000efc0e5bd2bc4b869a698af9ef5697685cb992
Jay Howard <hello@jaydanhoward.com>
2023-08-22 01:28:36 -0700

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 set -m
 
 echo "Starting netdata"
+chmod +x /tmp/netdata-kickstart.sh
 /tmp/netdata-kickstart.sh --claim-token $NETDATA_CLAIM_TOKEN --claim-rooms a707fdee-792b-4e43-96b0-4a1a62771462 --claim-url https://app.netdata.cloud
 
 
