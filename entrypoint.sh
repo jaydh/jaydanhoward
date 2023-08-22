897f31558a1ccd9fab800210c94e91a43b11cba2
Jay Howard <hello@jaydanhoward.com>
2023-08-22 00:36:34 -0700

    fix room with uuid

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -m
 
+echo "Starting netdata"
+netdata -d -u root -W "claim -token=$NETDATA_CLAIM_TOKEN rooms=a707fdee-792b-4e43-96b0-4a1a62771462 url=https://app.netdata.cloud" 
+
 
 echo "Starting nginx"
 service nginx start &
 
-echo "Starting netdata"
-netdata -d -u root -W "claim -token=$NETDATA_CLAIM_TOKEN rooms=jaydanhoward url=https://app.netdata.cloud" &
-
 echo "Starting HTTP server"
-./jaydanhoward 
+./jaydanhoward
