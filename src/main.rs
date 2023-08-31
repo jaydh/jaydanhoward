46f0e9772244090f5bb5e9d276070476368e2643
Jay Howard <hello@jaydanhoward.com>
2023-08-30 21:48:31 -0700

    add back empy main

--- src/main.rs
+++ src/main.rs
@@ -17,3 +17,11 @@ async fn favicon(
         "{site_root}/favicon.ico"
     ))?)
 }
+
+#[cfg(not(any(feature = "ssr", feature = "csr")))]
+pub fn main() {
+    // no client-side main function
+    // unless we want this to work with e.g., Trunk for pure client-side testing
+    // see lib.rs for hydration function instead
+    // see optional feature `csr` instead
+}
