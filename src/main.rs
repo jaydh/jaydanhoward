5bfe9fae17b42297ebdb2a222bade27797c6380b
Jay Howard <hello@jaydanhoward.com>
2023-09-05 08:03:14 -0700

    delete comment

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,4 @@ async fn favicon(
 }
 
 #[cfg(not(any(feature = "ssr", feature = "csr")))]
-pub fn main() {
-    // no client-side main function
-    // unless we want this to work with e.g., Trunk for pure client-side testing
-    // see lib.rs for hydration function instead
-    // see optional feature `csr` instead
-}
+pub fn main() {}
