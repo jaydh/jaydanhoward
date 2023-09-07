612f9cd307837802f27e7e5dcb932ece84af1649
Jay Howard <hello@jaydanhoward.com>
2023-09-06 22:34:25 -0700

    source_anchor component

---
+++ src/components/source_anchor.rs
@@ -0,0 +1,18 @@
+use leptos::*;
+
+#[component]
+pub fn SourceAnchor(cx: Scope, href: &'static str) -> impl IntoView {
+    view! {
+            cx,
+            <Show when= move || href == "#[git]" fallback = |cx| view! { cx, <div /> }>
+                <div class="group">
+                    <a class="fixed bottom-12 right-12 fas fa-code" href="#[git]" target="_blank" rel="noreferrer" />
+                    <span
+                      class="fixed bottom-16 right-12 pointer-events-none opacity-0 transition-opacity group-hover:opacity-100"
+                    >
+                           Self-Referencing Source Code
+                    </span>
+                </div>
+            </Show>
+    }
+}
