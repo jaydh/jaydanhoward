4eccb2ba75dc2fda4eec2069f312cbd33533cff6
Jay Howard <hello@jaydanhoward.com>
2024-03-24 00:04:15 -0700

    put lable on source anchor

--- src/components/source_anchor.rs
+++ src/components/source_anchor.rs
@@ -3,8 +3,14 @@ use leptos::*;
 #[component]
 pub fn SourceAnchor(href: &'static str) -> impl IntoView {
     view! {
-        <div class="group fixed bottom-12 right-12" aria-hidden="true">
-            <a class="fas fa-code" href=href target="_blank" rel="noreferrer"></a>
+        <div class="group fixed bottom-12 right-12">
+            <a
+                class="fas fa-code"
+                href=href
+                target="_blank"
+                rel="noreferrer"
+                aria-label="source_anchor"
+            ></a>
             <span class="absolute -top-20 -left-20 pointer-events-none opacity-0 transition-opacity group-hover:opacity-100">
                 Self-Referencing Source Code
             </span>
