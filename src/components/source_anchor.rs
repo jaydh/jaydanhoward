16c67efcd87743723ee8aef5b1f7f526497f18d1
Jay Howard <hello@jaydanhoward.com>
2024-03-23 23:34:04 -0700

    move hidden aria

--- src/components/source_anchor.rs
+++ src/components/source_anchor.rs
@@ -3,17 +3,17 @@ use leptos::*;
 #[component]
 pub fn SourceAnchor(href: &'static str) -> impl IntoView {
     view! {
-        <div class="group fixed bottom-12 right-12 ">
-            <a
-                class="fas fa-code"
-                href=href
-                target="_blank"
-                rel="noreferrer"
-                aria-hidden="true"
-            ></a>
-            <span class="absolute -top-20 -left-20 pointer-events-none opacity-0 transition-opacity group-hover:opacity-100">
-                Self-Referencing Source Code
-            </span>
-        </div>
-    }
+            <div class="group fixed bottom-12 right-12" aria-hidden="true"
+    >
+                <a
+                    class="fas fa-code"
+                    href=href
+                    target="_blank"
+                    rel="noreferrer"
+                ></a>
+                <span class="absolute -top-20 -left-20 pointer-events-none opacity-0 transition-opacity group-hover:opacity-100">
+                    Self-Referencing Source Code
+                </span>
+            </div>
+        }
 }
