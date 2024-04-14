f8cbb87f06d6eb66707dc04e5baf7a77105f15e7
Jay Howard <hello@jaydanhoward.com>
2024-04-14 11:50:24 -0700

    add aria hidden on root source anchor

--- src/components/source_anchor.rs
+++ src/components/source_anchor.rs
@@ -3,7 +3,7 @@ use leptos::*;
 #[component]
 pub fn SourceAnchor(href: &'static str) -> impl IntoView {
     view! {
-        <div class="group fixed bottom-12 right-12">
+        <div class="group fixed bottom-12 right-12 aria-hidden='true' ">
             <a
                 class="fas fa-code"
                 href=href
