a47087673c527cb9e3c2126db05f00b2d4a8d7a7
Jay Howard <hello@jaydanhoward.com>
2024-08-25 17:10:02 -0700

    env var all the endpoints

--- lighthouse/entrypoint.sh
+++ lighthouse/entrypoint.sh
@@ -5,9 +5,9 @@ MAX_RETRIES=10  # Maximum number of retries
 RETRY_INTERVAL=5  # Time interval between retries in seconds
 retry_count=0
 
-echo "Starting lighthouse worker with target $LEPTOS_SITE_ADDR" 
+echo "Starting lighthouse worker with target $LEPTOS_SITE_TARGET_ADDR" 
 while [ $retry_count -lt $MAX_RETRIES ]; do
-    response_code=$(curl -s -o /dev/null -w "%{http_code}" $LEPTOS_SITE_ADDR/health_check)
+    response_code=$(curl -s -o /dev/null -w "%{http_code}" $LEPTOS_SITE_HEALTH_ADDR)
     
     if [ "$response_code" -eq 200 ]; then
         echo "Health check successful (HTTP 200)."
@@ -24,6 +24,6 @@ if [ $retry_count -eq $MAX_RETRIES ]; then
     exit 1
 fi
 
-lighthouse --output-path lighthouse.html --chrome-flags="--headless --no-sandbox" $LEPTOS_SITE_ADDR --no-enable-error-reporting
-curl -u jay:$LIGHTHOUSE_UPDATE_TOKEN --location "$LEPTOS_SITE_ADDR/api/lighthouse" --form 'file=@"/home/chrome/lighthouse.html"'
+lighthouse --output-path lighthouse.html --chrome-flags="--headless --no-sandbox" $LEPTOS_SITE_TARGET_ADDR --no-enable-error-reporting
+curl -u jay:$LIGHTHOUSE_UPDATE_TOKEN --location "$LEPTOS_SITE_API_ADDR" --form 'file=@"/home/chrome/lighthouse.html"'
 exit 0
