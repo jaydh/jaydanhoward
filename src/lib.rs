4004dea75be9b7339270243b06cfa7c51d7f6a82
Jay Howard <hello@jaydanhoward.com>
2024-12-24 16:18:23 -0800

    cleaner hydrate mount

--- src/lib.rs
+++ src/lib.rs
@@ -7,21 +7,14 @@ pub mod routes;
 pub mod startup;
 pub mod telemtry;
 
-cfg_if! {
-if #[cfg(feature = "hydrate")] {
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
+    use app::*;
 
-  use wasm_bindgen::prelude::wasm_bindgen;
-  use components::App;
+    // initializes logging using the `log` crate
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
 
-    #[wasm_bindgen]
-    pub fn hydrate() {
-      use leptos::*;
-
-      console_error_panic_hook::set_once();
-
-      leptos::prelude::mount_to_body(move || {
-          view! { <App/> }
-      });
-    }
-}
+    leptos::mount::hydrate_body(App);
 }
